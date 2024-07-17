7d132461ee62aca19ebae8d868084ea8c07b1ec7
Phrompong.kht <Phrompong.kht@gmail.com>
2024-07-17 16:31:17 +0700

    chore: Update contact modal layout and translation feature

--- app/components/modals/contact.tsx
+++ app/components/modals/contact.tsx
@@ -7,8 +7,22 @@ const ContactModal: React.FC<ModalProps> = ({}) => {
     <div className="fixed z-10 pt-24 w-full h-full overflow-auto bg-black bg-opacity-40 text-white">
       <div className="relative top-1/3">
         <div className="flex justify-center">
-          <div className="flex flex-col border p-10 rounded-lg bg-modal">
-            <span>วิธีใช้งาน</span>
+          <div className="flex flex-col border w-2/4 p-10 rounded-lg bg-modal gap-10">
+            <span className="flex justify-center">วิธีใช้งาน</span>
+
+            <div className="flex flex-row border p-10 gap-10 rounded-lg bg-translated">
+              <div className="flex flex-col">
+                <span>
+                  1. คัดลอก / พิมพ์ ข้อความหรือตัวอังษรที่ลืมเปลี่ยนภาษา
+                  ทั้งภาษาไทย หรือภาษาอังกฤษลงในช่อง
+                </span>
+              </div>
+              <div className="flex flex-col">
+                <span>
+                  2. แจ๋วจะทำการแปลเป็นคำที่ถูกต้องให้ และสามารถนำไปใช้ได้เลย
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
